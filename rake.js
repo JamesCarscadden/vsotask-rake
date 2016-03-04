6675f91aad0364327cb0c6f882dc73175b2d7a82
James Carscadden <james@carscadden.org>
2016-03-04 13:14:28 -0500

    move most settings into advanced section. Add some new options for environment and db connection

--- rake.js
+++ rake.js
@@ -1,13 +1,25 @@
 var path = require('path');
 var tl = require('vso-task-lib');
 
-tl.setVariable('RAILS_ENV', 'test');
+// Find rake and get the tasks that it will run
 var rake = new tl.ToolRunner(tl.which('rake', true));
 var cmd = tl.getInput('command', true);
-var bundleexec = tl.getInput('usebundle', false);
-
 rake.arg(cmd);
 
+// if dbcmd has been set then add it to the environment
+var dbcmd = tl.getInput('dbconnection', false);
+if (dbcmd) {
+    tl.setVariable('DATABASE_URL', dbcmd);
+}
+
+// if environment has been set then add it to the environment
+var railsenv = tl.getInput('railsenvironment', false)
+if (railsenv) {
+    tl.setVariable('RAILS_ENV', railsenv);
+}
+
+// Determine if we're going to use bundle exec
+var bundleexec = tl.getInput('usebundle', false);
 var bundle = new tl.ToolRunner(tl.which('bundle', true));
 bundle.arg('exec');
 bundle.arg('rake');
